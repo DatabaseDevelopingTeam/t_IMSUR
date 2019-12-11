8fb1eb330b5af2ce4a6e80ad2d4e9f71c5e7bbe3
william <1584075475@qq.com>
2019-12-11 16:24:43 +0800

    move to team

--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def hello(request):
-    return HttpResponse('Hello,world')
+
+def index(request):
+    context = {'hellostr': 'Hello World'}
+    return render(request, 'index.html', context)
