55eae1cadf2175e8e41fe612679d366a18530b4f
CQJTUWilliam <1584075475@qq.com>
2019-12-16 11:02:05 +0800

    重新配置了settings.py

--- IMSUR/settings.py
+++ IMSUR/settings.py
@@ -1,23 +1,37 @@
 import os
+import socket
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '@t83x!+q+q!g6g8ms*bq2q2(xtr=16tna7(%96su5&$&221#rs'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# 环境部署选择
+def hostname():
+    sys = os.name
+    if sys == 'nt':
+        hostname = os.getenv('computername')
+        return hostname
+    elif sys == 'posix':
+        host = os.popen('echo $HOSTNAME')
+        try:
+            hostname = host.read()
+            return hostname
+        finally:
+            host.close()
+    else:
+        raise RuntimeError('Unkwon hostname')
 
-ALLOWED_HOSTS = []
 
+if socket.gethostname().lower() == hostname().lower():
+    DEBUG = TEMPLATE_DEBUG = True
+    ALLOWED_HOSTS = []
+else:
+    DEBUG = TEMPLATE_DEBUG = False
+    ALLOWED_HOSTS = [
+        '0.0.0.0',
+    ]
 
-# Application definition
-
+# 应用定义
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -25,9 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'HelloWorld'
 ]
 
+MY_APPs = [
+    'HelloWorld',
+]
+INSTALLED_APPS += MY_APPs
+
+#中间件
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -38,13 +57,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+#根路由
 ROOT_URLCONF = 'IMSUR.urls'
 
+#模板配置
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -59,17 +79,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'IMSUR.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+# mysql数据库配置
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'web_test',
+        'USER': 'root',
+        'PASSWORD': 'Wxp13620060844.',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -89,21 +109,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
 LANGUAGE_CODE = 'zh-hans'
 
-# TIME_ZONE = 'UTC'
 TIME_ZONE = 'Asia/Shanghai'
+
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
