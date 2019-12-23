0061e2543b06eb6292ea95d07c07946774e8b195
hk-ccc <1633593896@qq.com>
2019-12-23 11:53:29 +0800

    添加日常巡查表

--- patrolManagementUI/admin.py
+++ patrolManagementUI/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import 定期任务
+from .models import 定期巡查
+from .models import 日常巡查
 
 # Register your models here.
 
 
-@admin.register(定期任务)
+@admin.register(定期巡查)
 class RegularTaskAdmin(admin.ModelAdmin):
     # 要展示的字段
     list_display = [id, '巡查日期']
@@ -13,9 +14,14 @@ class RegularTaskAdmin(admin.ModelAdmin):
     list_filter = ['巡查日期']
     # 可以搜索的字段
     search_fields = ['巡查日期']
-    # 可以在全表中直接修改的字段
-    # list_editable = ['姓名', '性别', '职能']
-    # 排除字段
-    # 只可读字段
-    # readonly_fields = ['工号', '密码']
-    # list_editable = ['姓名','性别']
+
+
+@admin.register(日常巡查)
+class DailyTaskAdmin(admin.ModelAdmin):
+    #要展示的字段
+    list_display=[id,'巡查日期']
+    #可以点击的字段
+    list_display_links=['巡查日期']
+    list_filter=['巡查日期']
+    #可以搜索的字段
+    search_fields=['巡查日期']
--- patrolManagementUI/models.py
+++ patrolManagementUI/models.py
@@ -4,10 +4,17 @@ from municipalManagementUI import models as model_municipalManagementUI
 # Create your models here.
 
 
-class 定期任务(models.Model):
+class 定期巡查(models.Model):
     巡查日期=models.DateField('巡查日期')
     道路基本档案=models.ForeignKey(model_municipalManagementUI.道路基本档案,on_delete=models.CASCADE,null=True)
 
     class Meta:
-        verbose_name = '定期任务'
-        verbose_name_plural = '定期任务'
+        verbose_name = '定期巡查'
+        verbose_name_plural = '定期巡查'
+
+class 日常巡查(models.Model):
+    巡查日期=models.DateField('巡查日期')
+    道路基本档案=models.ForeignKey(model_municipalManagementUI.道路基本档案,on_delete=models.CASCADE,null=True)
+    class Meta:
+        verbose_name='日常巡查'
+        verbose_name_plural='日常巡查'
