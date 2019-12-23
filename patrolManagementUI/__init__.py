f89e78f7ee0e0ac835ec95137f69c2bbf210c88c
hk-ccc <1633593896@qq.com>
2019-12-23 13:57:19 +0800

    巡查管理后台信息标题栏

---
+++ patrolManagementUI/__init__.py
@@ -0,0 +1,1 @@
+default_app_config='patrolManagementUI.apps.PatrolmanagementuiConfig'
--- patrolManagementUI/models.py
+++ patrolManagementUI/models.py
@@ -1,20 +1,23 @@
 from django.db import models
 from municipalManagementUI import models as model_municipalManagementUI
 
+
 # Create your models here.
 
 
 class 定期巡查(models.Model):
-    巡查日期=models.DateField('巡查日期')
-    道路基本档案=models.ForeignKey(model_municipalManagementUI.道路基本档案,on_delete=models.CASCADE,null=True)
+    巡查日期 = models.DateField('巡查日期')
+    道路基本档案 = models.ForeignKey(model_municipalManagementUI.道路基本档案, on_delete=models.CASCADE, null=True)
 
     class Meta:
         verbose_name = '定期巡查'
         verbose_name_plural = '定期巡查'
 
+
 class 日常巡查(models.Model):
-    巡查日期=models.DateField('巡查日期')
-    道路基本档案=models.ForeignKey(model_municipalManagementUI.道路基本档案,on_delete=models.CASCADE,null=True)
+    巡查日期 = models.DateField('巡查日期')
+    道路基本档案 = models.ForeignKey(model_municipalManagementUI.道路基本档案, on_delete=models.CASCADE, null=True)
+
     class Meta:
-        verbose_name='日常巡查'
-        verbose_name_plural='日常巡查'
+        verbose_name = '日常巡查'
+        verbose_name_plural = '日常巡查'
