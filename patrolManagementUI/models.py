1a3a06b7d08cf17730ffd622d32a57242cb81e8a
hk-ccc <1633593896@qq.com>
2019-12-23 11:18:18 +0800

    修改巡查表

--- patrolManagementUI/models.py
+++ patrolManagementUI/models.py
@@ -1,13 +1,12 @@
 from django.db import models
+from municipalManagementUI import models as model_municipalManagementUI
 
 # Create your models here.
 
 
 class 定期任务(models.Model):
     巡查日期=models.DateField('巡查日期')
-    #道路编号=models.ForeignKey(道路基本档案,verbose_name='巡查道路编号',related_name='roadNumber',on_delete=models.CASCADE)
-    #道路名称=models.ForeignKey(道路基本档案,verbose_name='巡查道路名称',on_delete=models.CASCADE,related_name='roadName')
-    #道路等级=models.ForeignKey(道路基本档案,verbose_name='巡查道路等级',on_delete=models.CASCADE,related_name='roadLevel')
+    道路基本档案=models.ForeignKey(model_municipalManagementUI.道路基本档案,on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = '定期任务'
