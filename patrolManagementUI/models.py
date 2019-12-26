f935e89e706abb1da5970aacdf2c94fe10b20730
hk-ccc <1633593896@qq.com>
2019-12-26 15:09:37 +0800

    优化日常巡查表的外键引用初步测试

--- patrolManagementUI/models.py
+++ patrolManagementUI/models.py
@@ -16,7 +16,9 @@ class 定期巡查(models.Model):
 
 class 日常巡查(models.Model):
     巡查日期 = models.DateField('巡查日期')
-    道路基本档案 = models.ForeignKey(model_municipalManagementUI.道路基本档案, on_delete=models.CASCADE, null=True)
+    道路编号 = models.ForeignKey(model_municipalManagementUI.道路基本档案,related_name='no',on_delete=models.CASCADE,null=True)
+    道路名称=models.ForeignKey(model_municipalManagementUI.道路基本档案,related_name='name',on_delete=models.CASCADE,null=True)
+    #道路基本档案 = models.ForeignKey(model_municipalManagementUI.道路基本档案, on_delete=models.CASCADE, null=True)
 
     class Meta:
         verbose_name = '日常巡查'
