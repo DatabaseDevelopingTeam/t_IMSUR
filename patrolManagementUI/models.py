fd790fa315c229adef92c576931c7f8f9be7f64b
hk-ccc <1633593896@qq.com>
2019-12-28 09:15:40 +0800

    巡查表修改maxlength

--- patrolManagementUI/models.py
+++ patrolManagementUI/models.py
@@ -8,7 +8,7 @@ from municipalManagementUI import models as model_municipalManagementUI
 class 定期巡查(models.Model):
     巡查日期 = models.DateField('巡查日期')
     巡查道路 = models.ForeignKey(model_municipalManagementUI.道路基本档案, on_delete=models.CASCADE, null=True)
-    巡查状态=models.CharField('巡查状态',choices=(('1','已巡查'),('2','未巡查')),default='未巡查')
+    巡查状态=models.CharField('巡查状态',max_length=20,choices=(('1','已巡查'),('2','未巡查')),default='未巡查')
 
     class Meta:
         verbose_name = '定期巡查'
@@ -18,7 +18,7 @@ class 定期巡查(models.Model):
 class 日常巡查(models.Model):
     巡查日期 = models.DateField('巡查日期')
     巡查道路 = models.ForeignKey(model_municipalManagementUI.道路基本档案,related_name='no',on_delete=models.CASCADE,null=True)
-    巡查状态 = models.CharField('巡查状态', choices=(('1', '已巡查'), ('2', '未巡查')), default='未巡查')
+    巡查状态 = models.CharField('巡查状态', max_length=20,choices=(('1', '已巡查'), ('2', '未巡查')), default='未巡查')
     #道路名称=models.ForeignKey(model_municipalManagementUI.道路基本档案,related_name='name',on_delete=models.CASCADE,null=True)
     #道路基本档案 = models.ForeignKey(model_municipalManagementUI.道路基本档案, on_delete=models.CASCADE, null=True)
 
