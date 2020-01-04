5727f849a2825fc2c56b2df8fe29f5fb62c6d1ac
hk-ccc <1633593896@qq.com>
2020-01-04 22:16:09 +0800

    完成自动生成巡查任务

--- patrolManagementUI/models.py
+++ patrolManagementUI/models.py
@@ -8,7 +8,7 @@ from municipalManagementUI import models as model_municipalManagementUI
 class 定期巡查任务(models.Model):
     巡查日期 = models.DateField('巡查日期')
     巡查道路 = models.ForeignKey(model_municipalManagementUI.道路基本档案, on_delete=models.CASCADE, null=True)
-    巡查状态 = models.CharField('巡查状态', max_length=20, choices=(('1', '未巡查'), ('2', '已巡查')), default='未巡查')
+    巡查状态 = models.CharField('巡查状态', max_length=20, choices=(('1', '未巡查'), ('2', '已巡查')), default='1')
 
     class Meta:
         verbose_name = '定期巡查任务'
--- patrolManagementUI/views.py
+++ patrolManagementUI/views.py
@@ -18,12 +18,70 @@ def patrolManagementUI(request):
         'employee_id': user.工号,
         'position': '市政道路管理' if user.职能 == '1' else '道路巡查养护'
     }
+    time = datetime.date.today()
+    # 生成巡查任务
+    Roads = models.道路基本档案.objects.all()
+    for Road in Roads:
+        Tasks = Road.no.all()
+        # 若相关的日常巡查任务已经存在
+        if Tasks.exists():
+            for Task in Tasks:
+                if Task.巡查状态 == '2':
+                    if Task.巡查道路.道路等级.道路等级 == '1':
+                        time = Task.巡查日期 + datetime.timedelta(days=1)
+                    if Task.巡查道路.道路等级.道路等级 == '2':
+                        time = Task.巡查日期 + datetime.timedelta(days=2)
+                    if Task.巡查道路.道路等级.道路等级 == '3':
+                        time = Task.巡查日期 + datetime.timedelta(days=3)
+                    if time < datetime.date.today():
+                        time = datetime.date.today()
+                    Task.巡查日期 = time
+                    Task.巡查状态 = '1'
+                    Task.save()
+                else:
+                    if Task.巡查日期 < datetime.date.today():
+                        time = datetime.date.today()
+                        Task.巡查日期 = time
+                        Task.save()
+        else:
+            time = datetime.date.today()
+            DailyTask = model2.日常巡查任务(巡查道路=Road, 巡查日期=time, 巡查状态='1')
+            DailyTask.save()
+
+        RegularTasks = Road.定期巡查任务_set.all()
+        # 若相关的定期巡查任务已经存在
+        if RegularTasks.exists():
+            for RegularTask in RegularTasks:
+                if RegularTask.巡查状态 == '2':
+                    if RegularTask.巡查道路.道路等级.道路等级 == '1':
+                        time = RegularTask.巡查日期 + datetime.timedelta(days=730)
+                        RegularTask.巡查日期 = time
+                    if RegularTask.巡查道路.道路等级.道路等级 == '2':
+                        time = RegularTask.巡查日期 + datetime.timedelta(days=1095)
+                        RegularTask.巡查日期 = time
+                    if RegularTask.巡查道路.道路等级.道路等级 == '3':
+                        time = RegularTask.巡查日期 + datetime.timedelta(days=1460)
+                    if time < datetime.date.today():
+                        time = datetime.date.today()
+                    RegularTask.巡查日期 = time
+                    RegularTask.巡查状态 = '1'
+                    RegularTask.save()
+                else:
+                    if RegularTask.巡查日期 < datetime.date.today():
+                        time = datetime.date.today()
+                        RegularTask.巡查日期 = time
+                        RegularTask.save()
+        else:
+            time = datetime.date.today()
+            DQTask = model2.定期巡查任务(巡查道路=Road, 巡查日期=time, 巡查状态='1')
+            DQTask.save()
+
     return render(request, 'patrolManagement.html', context=user_dict)
 
 
 def patrolMap(request):
     today = datetime.date.today()
-    Tasks = model2.日常巡查任务.objects.filter(巡查日期=today,巡查状态='1')
+    Tasks = model2.日常巡查任务.objects.filter(巡查日期=today, 巡查状态='1')
     return render(request, 'patrolMap.html', {"Tasks": Tasks})
 
 
@@ -57,70 +115,69 @@ def getRoadsLatlng(request):
 @csrf_exempt
 def getRoadInfoPopup(request):
     roadId = request.POST.get('roadId')
-    dicRoad=models.道路基本档案.objects.get(道路编号=roadId)
-    return render(request, 'RoadBasicInfoPopup.html', {'Road':dicRoad})
+    dicRoad = models.道路基本档案.objects.get(道路编号=roadId)
+    return render(request, 'RoadBasicInfoPopup.html', {'Road': dicRoad})
 
 
 @csrf_exempt
 def setupModalView(request):
     Info = {}
-    roadId=request.POST.get('roadId')
-    Road=models.道路基本档案.objects.get(道路编号=roadId)
-    employee_id=request.COOKIES.get('employee_id')
+    roadId = request.POST.get('roadId')
+    Road = models.道路基本档案.objects.get(道路编号=roadId)
+    employee_id = request.COOKIES.get('employee_id')
     user = models.职工.objects.get(工号=employee_id)
-    Info['employeeId']=user.工号
-    Info['employeeName']=user.姓名
-    CarRoad=models.车行道.objects.get(道路编号=roadId)
-    Roadtype=models.路面类型.objects.get(路面类型=CarRoad.路面类型.路面类型)
-    DamageTypes=Roadtype.rn路面类型.all()
-    i=1;
+    Info['employeeId'] = user.工号
+    Info['employeeName'] = user.姓名
+    CarRoad = models.车行道.objects.get(道路编号=roadId)
+    Roadtype = models.路面类型.objects.get(路面类型=CarRoad.路面类型.路面类型)
+    DamageTypes = Roadtype.rn路面类型.all()
+    i = 1;
     for DamageType in DamageTypes:
-        Info['%s'%(i)]=DamageType.损坏类型
-        i=i+1
-    Info['roadId']=Road.道路编号
-    Info['roadName']=Road.道路名称
-    Info['roadType']=CarRoad.路面类型.路面类型
-    Info['length']='%s' % (i)
-    return JsonResponse(Info,safe=False)
+        Info['%s' % (i)] = DamageType.损坏类型
+        i = i + 1
+    Info['roadId'] = Road.道路编号
+    Info['roadName'] = Road.道路名称
+    Info['roadType'] = CarRoad.路面类型.路面类型
+    Info['length'] = '%s' % (i)
+    return JsonResponse(Info, safe=False)
 
 
 @csrf_exempt
 def AddDailyPatrolRecord(request):
-    employeeId=request.COOKIES.get('employee_id')
-    user=models.职工.objects.get(工号=employeeId)
-    roadId=request.POST.get('roadId')
-    print('roadId:'+str(roadId))
-    road=models.道路基本档案.objects.get(道路编号=roadId)
-    time=datetime.date.today()
-    #建立日常巡查记录档案
-    DailyPatrolRecord=model2.日常巡查记录(巡查人员=user,道路编号=road,巡查日期=time)
+    employeeId = request.COOKIES.get('employee_id')
+    user = models.职工.objects.get(工号=employeeId)
+    roadId = request.POST.get('roadId')
+    road = models.道路基本档案.objects.get(道路编号=roadId)
+    time = datetime.date.today()
+    # 建立日常巡查记录档案
+    DailyPatrolRecord = model2.日常巡查记录(巡查人员=user, 道路编号=road, 巡查日期=time)
     DailyPatrolRecord.save()
-    roadType=request.POST.get('roadType')
-    #获取路面类型表的对象
-    roadTypeObject=models.路面类型.objects.get(路面类型=roadType)
-    #建立日常巡查损坏记录档案
+    roadType = request.POST.get('roadType')
+    # 获取路面类型表的对象
+    roadTypeObject = models.路面类型.objects.get(路面类型=roadType)
+    # 建立日常巡查损坏记录档案
     # infos = request.POST.get('infos')
-    infos=json.loads(request.POST.get('infos'))
+    infos = json.loads(request.POST.get('infos'))
     for v in infos.values():
-        damageType=v['damageType']
-        damageDetail=v['damageDetail']
-        note=v['note']
-        damageTypeObject=models.路面损坏类型.objects.get(要引用的路面类型=roadTypeObject,损坏类型=damageType)
-        DailyPatrolDamageRecordObject=model2.日常巡查损害记录(日常巡查记录编号=DailyPatrolRecord,损坏类型=
-                                                      damageTypeObject,损坏位置及情况描述=damageDetail,备注=note)
+        damageType = v['damageType']
+        damageDetail = v['damageDetail']
+        note = v['note']
+        damageTypeObject = models.路面损坏类型.objects.get(要引用的路面类型=roadTypeObject, 损坏类型=damageType)
+        DailyPatrolDamageRecordObject = model2.日常巡查损害记录(日常巡查记录编号=DailyPatrolRecord, 损坏类型=
+        damageTypeObject, 损坏位置及情况描述=damageDetail, 备注=note)
         DailyPatrolDamageRecordObject.save()
-    DailyTask=model2.日常巡查任务.objects.get(巡查日期=time,巡查道路=road,巡查状态='1')
-    DailyTask.巡查状态='2'
+    DailyTask = model2.日常巡查任务.objects.get(巡查日期=time, 巡查道路=road, 巡查状态='1')
+    DailyTask.巡查状态 = '2'
     DailyTask.save()
-    Location=road.getLatlng()
-    return JsonResponse(Location,safe=False)
+    Location = road.getLatlng()
+    return JsonResponse(Location, safe=False)
 
 
 # 定期巡查
 def patrolMap2(request):
-    today=datetime.date.today()
-    Tasks=model2.定期巡查任务.objects.filter(巡查日期=today,巡查状态='1')
-    return render(request,'patrolMap2.html',{"Tasks": Tasks})
+    today = datetime.date.today()
+    Tasks = model2.定期巡查任务.objects.filter(巡查日期=today, 巡查状态='1')
+    return render(request, 'patrolMap2.html', {"Tasks": Tasks})
 
 
 # 返回所有道路对应的基本信息
@@ -152,47 +209,46 @@ def getRoadsLatlng2(request):
 @csrf_exempt
 def setupModalView2(request):
     Info = {}
-    roadId=request.POST.get('roadId')
-    Road=models.道路基本档案.objects.get(道路编号=roadId)
-    employee_id=request.COOKIES.get('employee_id')
+    roadId = request.POST.get('roadId')
+    Road = models.道路基本档案.objects.get(道路编号=roadId)
+    employee_id = request.COOKIES.get('employee_id')
     user = models.职工.objects.get(工号=employee_id)
-    Info['employeeId']=user.工号
-    Info['employeeName']=user.姓名
-    CarRoad=models.车行道.objects.get(道路编号=roadId)
-    Roadtype=models.路面类型.objects.get(路面类型=CarRoad.路面类型.路面类型)
-    DamageTypes=Roadtype.rn路面类型.all()
-    i=1;
+    Info['employeeId'] = user.工号
+    Info['employeeName'] = user.姓名
+    CarRoad = models.车行道.objects.get(道路编号=roadId)
+    Roadtype = models.路面类型.objects.get(路面类型=CarRoad.路面类型.路面类型)
+    DamageTypes = Roadtype.rn路面类型.all()
+    i = 1;
     for DamageType in DamageTypes:
-        Info['%s'%(i)]=DamageType.损坏类型
-        i=i+1
-    Info['roadId']=Road.道路编号
-    Info['roadName']=Road.道路名称
-    Info['roadType']=CarRoad.路面类型.路面类型
-    Info['length']='%s' % (i)
-    return JsonResponse(Info,safe=False)
+        Info['%s' % (i)] = DamageType.损坏类型
+        i = i + 1
+    Info['roadId'] = Road.道路编号
+    Info['roadName'] = Road.道路名称
+    Info['roadType'] = CarRoad.路面类型.路面类型
+    Info['length'] = '%s' % (i)
+    return JsonResponse(Info, safe=False)
 
 
 @csrf_exempt
 def AddDailyPatrolRecord2(request):
-    employeeId=request.COOKIES.get('employee_id')
-    user=models.职工.objects.get(工号=employeeId)
-    roadId=request.POST.get('roadId')
-    print('roadId:'+str(roadId))
-    road=models.道路基本档案.objects.get(道路编号=roadId)
-    time=datetime.date.today()
+    employeeId = request.COOKIES.get('employee_id')
+    user = models.职工.objects.get(工号=employeeId)
+    roadId = request.POST.get('roadId')
+    road = models.道路基本档案.objects.get(道路编号=roadId)
+    time = datetime.date.today()
     # 建立定期巡查记录档案
-    RegularPatrolRecord=model2.定期检测记录(巡查人员=user,道路编号=road,巡查日期=time)
+    RegularPatrolRecord = model2.定期检测记录(巡查人员=user, 道路编号=road, 巡查日期=time)
     RegularPatrolRecord.save()
-    roadType=request.POST.get('roadType')
+    roadType = request.POST.get('roadType')
     # 建立平整度记录档案
-    IRI=request.POST.get('IRI')
-    IRINote=request.POST.get('IRINote')
-    IRIRecord=model2.路面平整度检测记录(定期检查记录编号=RegularPatrolRecord,IRI=IRI,备注=IRINote)
+    IRI = request.POST.get('IRI')
+    IRINote = request.POST.get('IRINote')
+    IRIRecord = model2.路面平整度检测记录(定期检查记录编号=RegularPatrolRecord, IRI=IRI, 备注=IRINote)
     IRIRecord.save()
     # 获取路面类型表的对象
-    roadTypeObject=models.路面类型.objects.get(路面类型=roadType)
+    roadTypeObject = models.路面类型.objects.get(路面类型=roadType)
     # 建立定期巡查损坏记录档案
-    infos=json.loads(request.POST.get('infos'))
+    infos = json.loads(request.POST.get('infos'))
     for v in infos.values():
         damageType = v['damageType']
         beginLocation = v['beginLocation']
@@ -203,13 +259,14 @@ def AddDailyPatrolRecord2(request):
         damageHeight = v['damageHeight']
         damageDetail = v['damageDetail']
         damageNote = v['damageNote']
-        damageTypeObject=models.路面损坏类型.objects.get(要引用的路面类型=roadTypeObject,损坏类型=damageType)
-        RegularPatrolDamageRecordObject=model2.路面定期检查损害记录(定期检查记录编号=RegularPatrolRecord,损坏类型=
-          damageTypeObject,起止位置=beginLocation,检查总长=TotalLength,检查总宽=TotalWidth,损坏长=damageLength,
-            损坏宽=damageWidth,损坏高=damageHeight,损坏位置及情况描述=damageDetail,备注=damageNote)
+        damageTypeObject = models.路面损坏类型.objects.get(要引用的路面类型=roadTypeObject, 损坏类型=damageType)
+        RegularPatrolDamageRecordObject = model2.路面定期检查损害记录(定期检查记录编号=RegularPatrolRecord, 损坏类型=
+        damageTypeObject, 起止位置=beginLocation, 检查总长=TotalLength, 检查总宽=TotalWidth, 损坏长=damageLength,
+                                                            损坏宽=damageWidth, 损坏高=damageHeight, 损坏位置及情况描述=damageDetail,
+                                                            备注=damageNote)
         RegularPatrolDamageRecordObject.save()
-    RegularTask=model2.定期巡查任务.objects.get(巡查日期=time,巡查道路=road,巡查状态='1')
-    RegularTask.巡查状态='2'
+    RegularTask = model2.定期巡查任务.objects.get(巡查日期=time, 巡查道路=road, 巡查状态='1')
+    RegularTask.巡查状态 = '2'
     RegularTask.save()
-    Location=road.getLatlng()
-    return JsonResponse(Location,safe=False)
+    Location = road.getLatlng()
+    return JsonResponse(Location, safe=False)
